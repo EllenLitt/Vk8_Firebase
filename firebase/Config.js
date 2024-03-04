c1dbd3212fafbad0c22a11255950258fc9ba2af4
Ellen Littow <143400929+EllenLitt@users.noreply.github.com>
2024-03-04 13:11:33 +0200

    Update Config.js

--- firebase/Config.js
+++ firebase/Config.js
@@ -1,1 +1,23 @@
 
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, serverTimestamp, query, onSnapshot } from "firebase/firestore";
+
+// Initialize Firebase
+const firebaseConfig = {
+  //SISÄLTÖ POISTETTU
+};
+initializeApp(firebaseConfig);
+
+const firestore = getFirestore();
+
+const MESSAGES = 'messages';
+
+export {
+    firestore,
+    collection,
+    addDoc,
+    serverTimestamp,
+    query,
+    onSnapshot,
+    MESSAGES
+};
